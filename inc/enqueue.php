00c340f2224c694771805c2fe8402f49264d6ada
htmldiz <mark.tanasevich@gmail.com>
2019-04-15 14:12:23 +0300

    clear scripts

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,12 +11,7 @@ class Onetheme_enqueue
 		add_action( 'wp_enqueue_scripts', array( $this , 'enqueue_scripts' ) );
 	}
 	function enqueue_scripts(){
-		wp_enqueue_style( 'style-name', get_stylesheet_uri() );
-		wp_enqueue_script(
-			'webinarstheme-scripts',
-			get_template_directory_uri() . '/js/webinarstheme.script.js',
-			array('jquery')
-		);
+		wp_enqueue_style( 'style-onetheme', get_stylesheet_uri() );
 		if ( is_singular() ){
 			wp_enqueue_script( "comment-reply" );
 		}
