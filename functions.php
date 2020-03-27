dcc9e62ecc7113448878719415bce2b5a3c1496a
htmldiz <mark.tanasevich@gmail.com>
2020-03-28 00:00:52 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -38,9 +38,7 @@ class Onetheme_theme
         remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
         remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
         add_filter( 'tiny_mce_plugins', array($this,'disable_emojis_tinymce') );
-        // Remove comment script
         wp_deregister_script( 'comment-reply' );
-        // Remove rest_api
         remove_action( 'rest_api_init'            , 'wp_oembed_register_route' );
         remove_action( 'init'                     , 'rest_api_init' );
         remove_action( 'rest_api_init'            , 'rest_api_default_filters', 10 );
@@ -58,7 +56,6 @@ class Onetheme_theme
         add_filter( 'rewrite_rules_array'         , array($this,'disable_embeds_rewrites') );
         remove_filter( 'pre_oembed_result'        , 'wp_filter_pre_oembed_result', 10 );
         add_action( 'wp_default_scripts'          , array($this,'disable_embeds_remove_script_dependencies') );
-        // Remove feeds
         add_action('do_feed'                      , array($this,'wp_remove_rss_feed'), 1);
         add_action('do_feed_rdf'                  , array($this,'wp_remove_rss_feed'), 1);
         add_action('do_feed_rss'                  , array($this,'wp_remove_rss_feed'), 1);
@@ -66,6 +63,66 @@ class Onetheme_theme
         add_action('do_feed_atom'                 , array($this,'wp_remove_rss_feed'), 1);
         add_action('do_feed_rss2_comments'        , array($this,'wp_remove_rss_feed'), 1);
         add_action('do_feed_atom_comments'        , array($this,'wp_remove_rss_feed'), 1);
+        add_filter( 'woocommerce_enqueue_styles', array($this,'woocommerce_enqueue_styles') );
+        add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+        add_action('wp_print_scripts', array($this,'wp_print_scripts'), 100);
+        add_action('wp_print_styles', array($this,'wp_print_styles'), 100);
+    }
+    function wp_print_styles(){
+        wp_deregister_style("wp-block-editor");
+        wp_deregister_style("wp-block-library-theme");
+        wp_deregister_style("wp-block-library");
+        wp_deregister_style("wc-block-editor");
+        wp_deregister_style("select2");
+        wp_deregister_style("wc-block-style");
+    }
+    function wp_print_scripts($enqueue_styles){
+        global $wp_scripts;
+        wp_deregister_script( 'jquery' );
+        wp_deregister_script( 'accounting' );
+        wp_deregister_script( "wc-jquery-ui-touchpunch");
+        wp_deregister_script( "wc-price-slider");
+        wp_deregister_script( "wc-shared-settings");
+        wp_deregister_script( "wc-block-settings");
+        wp_deregister_script( "wc-blocks");
+        wp_deregister_script( "wc-vendors");
+        wp_deregister_script( "wc-handpicked-products");
+        wp_deregister_script( "wc-product-best-sellers");
+        wp_deregister_script( "wc-product-category");
+        wp_deregister_script( "wc-product-new");
+        wp_deregister_script( "wc-product-on-sale");
+        wp_deregister_script( "wc-product-top-rated");
+        wp_deregister_script( "wc-products-by-attribute");
+        wp_deregister_script( "wc-featured-product");
+        wp_deregister_script( "wc-featured-category");
+        wp_deregister_script( "wc-product-categories");
+        wp_deregister_script( "wc-product-tag");
+        wp_deregister_script( "wc-all-reviews");
+        wp_deregister_script( "wc-reviews-by-product");
+        wp_deregister_script( "wc-reviews-by-category");
+        wp_deregister_script( "wc-product-search");
+        wp_deregister_script( "wc-address-i18n");
+        wp_deregister_script( "wc-add-payment-method");
+        wp_deregister_script( "wc-cart");
+        wp_deregister_script( "wc-cart-fragments");
+        wp_deregister_script( "wc-checkout");
+        wp_deregister_script( "wc-country-select");
+        wp_deregister_script( "wc-credit-card-form");
+        wp_deregister_script( "wc-add-to-cart");
+        wp_deregister_script( "wc-add-to-cart-variation");
+        wp_deregister_script( "wc-geolocation");
+        wp_deregister_script( "wc-lost-password");
+        wp_deregister_script( "wc-password-strength-meter");
+        wp_deregister_script( "wc-single-product");
+//        echo "<pre>";
+//        var_dump($wp_scripts);
+//        echo "</pre>";
+    }
+    function woocommerce_enqueue_styles($enqueue_styles){
+        unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+        unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+        unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+        return $enqueue_styles;
     }
     function disable_emojis_tinymce($plugins){
         if ( is_array( $plugins ) ) {
